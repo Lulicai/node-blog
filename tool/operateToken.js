3425519017f1ffa4c829e74c6c256ae77295fbda
cailuli <>
2018-12-18 10:37:41 +0800

    token操作

--- tool/operateToken.js
+++ tool/operateToken.js
@@ -1,19 +1,19 @@
 const jwt = require('jsonwebtoken')
 
 module.exports = {
-    checkToken:function(token){
+    checkToken:function(ctx,next){
         if(token){
             let decoded = jwt.decode(token,'secret');
             // 如果过期了就重新登录
         // 验证token也需要优化
             if (token && decoded.exp <= Date.now() / 1000) {
                 ctx.body = {
-                code: 401,
-                message: "授权已经过期，请重新登陆"
+                    code: 401,
+                    message: "授权已经过期，请重新登陆"
                 };
                 return;
             }else{
-                return token;
+                next()
             }
         }
     },
@@ -21,7 +21,7 @@ module.exports = {
         var expiry = new Date();
         expiry.setDate(expiry.getDate()+7);//有效期设置为七天
         const token = jwt.sign({
-            name:name,
+            tel:tel,
             exp:parseInt(expiry.getTime()/1000)//除以1000以后表示的是秒数 到期时间
         },'secret')
         return token
